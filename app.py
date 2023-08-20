d64b61fdb6dd963e09fcd587067f1d00e3652495
Hae-rang <markvencent394@gmail.com>
2023-08-21 01:33:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ github_icon = current_dir / "assets" / "github_icon.png"
 #--- GENERAL SETTINGS ---#
 PAGE_TITLE = "Digital CV"
 PAGE_ICON = "♢"
-NAME = "Mark Vincent Estrada"
+NAME = "Mark Vincent Estradaa"
 CONTACT_NO = "09457194445 / 09474261330"
 ADDRESS = "Kapasigan, Pasig City"
 DESCRIPTION = """
